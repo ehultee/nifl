8b4b75bbe80e62a0bff6449b5a48de93e3841b5c
Lizz Ultee [air] <ehultee@umich.edu>
2020-10-09 16:45:53 -0400

    Move inversion routines to nifl_helper
    Functions `build_collection` and `computeCM` were always used to perform the velocity inversion, but defined within scripts.  Move to the helper module instead to clean up workflow.

--- nifl_helper.py
+++ nifl_helper.py
@@ -7,6 +7,7 @@ from math import sqrt
 import collections
 import numpy as np
 
+
 def Flowline_CSV(filename, has_width=False, flip_order=False):
     """Read in glacier flowlines in CSV format.
     Based on automatically-selected flowline format produced by EHU.
@@ -113,4 +114,83 @@ def ArcArray(flowline):
         else:
             arcarr.append(arcarr[-1]+ds(n, flowline))
     return arcarr
+
+## Set up design matrix and perform lasso regression, according to Bryan's documentation
+def build_collection(dates):
+    """
+    Function that creates a list of basis functions for a given datetime vector dates.
+    """
+    # Get date bounds
+    tstart, tend = dates[0], dates[-1]
+
+    # Initalize a collection and relevant basis functions
+    collection = ice.tseries.timefn.TimefnCollection()
+    bspl = ice.tseries.timefn.fnmap['bspline']
+    ispl = ice.tseries.timefn.fnmap['isplineset']
+    poly = ice.tseries.timefn.fnmap['poly']
+
+    # Add polynomial first for secular components
+    collection.append(poly(tref=tstart, order=1, units='years'))
+
+    # Use B-splines for seasonal (short-term) signals
+    Δtdec = 1.0 / 3.0 # years
+    Δt = datetime.timedelta(days=int(Δtdec*365))
+    t_current = tstart
+    while t_current <= tend:
+        collection.append(bspl(order=3, scale=Δtdec, units='years', tref=t_current))
+        t_current += Δt
+        
+    # Integrated B-slines for transient signals
+    # In general, we don't know the timescales of transients a prior
+    # Therefore, we add integrated B-splines of various timescales where the
+    # timescale is controlled by the 'nspl' value (this means to divide the time
+    # vector into 'nspl' equally spaced spline center locations)
+    for nspl in [128, 64, 32, 16, 8, 4]:
+        collection.append(ispl(order=3, num=nspl, units='years', tmin=tstart, tmax=tend))
+    
+    # Done
+    return collection
+
+# Build a priori covariance matrix, mainly for repeating B-splines
+def computeCm(collection, b_spl_sigma=0.5):
+    from scipy.linalg import block_diag
+
+    # Define some prior sigmas (large prior sigmas for secular and transient)
+    sigma_secular = 100.0
+    sigma_bspl = b_spl_sigma
+    sigma_ispl = 100.0
+
+    # Get lengths of different model partitions
+    fnParts = ice.tseries.timefn.getFunctionTypes(collection)
+    nBspl = len(fnParts['seasonal'])
+    nIspl = len(fnParts['transient'])
+    nSecular = len(fnParts['secular'])
+
+    # Diagonal prior for secular components
+    C_sec = sigma_secular**2 * np.eye(nSecular)
+
+    # Get decimal times of B-spline centers   
+    tdec = []
+    for basis in collection.data:
+        if basis.fnname == 'BSpline':
+            tdec.append(ice.datestr2tdec(pydtime=basis.tref))
+    tdec = np.array(tdec)
+
+    # Correlation with splines w/ similar knot times
+    # Decaying covariance w/ time
+    tau = 1.0
+    C_bspl = np.zeros((nBspl, nBspl))
+    for i in range(nBspl):
+        weight_decay = np.exp(-1.0 * np.abs(tdec - tdec[i]) / tau)
+        ind = np.zeros((nBspl,), dtype=bool)
+        ind[i::5] = True
+        ind[i::-5] = True
+        C_bspl[i,ind] = weight_decay[ind]
+    C_bspl *= sigma_bspl**2
+
+    # Diagonal prior for integrated B-splines
+    C_ispl = sigma_ispl**2 * np.eye(nIspl)
+
+    return block_diag(C_sec, C_bspl, C_ispl)
+
     
