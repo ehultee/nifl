32584636537b8e86c5c8abcb214b8c3b17eeca74
Lizz Ultee [air] <ehultee@umich.edu>
2020-10-09 16:41:14 -0400

    Pull velocity from stack along flowlines
    Identify upstream max in velocity coverage along flowline.  Pull velocity from a list of evenly-spaced points in this region.

--- pull_felikson_lines.py
+++ pull_felikson_lines.py
@@ -10,6 +10,7 @@ Test load-in of Felikson flowlines
 from netCDF4 import Dataset
 import numpy as np
 import matplotlib.pyplot as plt
+import iceutils as ice
 
 ## Pull single flowline from Denis Felikson
 fp1 = '/Users/lizz/Documents/GitHub/Data_unsynced/Felikson-flowlines/netcdfs/glaciera199.nc'
@@ -29,3 +30,14 @@ s2 = ncfile2['flowline05']['geometry']['surface']['GIMP']['nominal'].variables['
 b2 = ncfile2['flowline05']['geometry']['bed']['BedMachine']['nominal'].variables['h'][:] # BedMachine v3
 dh2 = ncfile2['flowline05']['dh']['GIMP-Arctic']['nominal']['dh'][:]
 # ncfile2.close()
+
+## Set up combined hdf5 stack
+fpath='/Users/lizz/Documents/GitHub/Data_unsynced/Gld-Stack/'
+hel_stack = ice.MagStack(files=[fpath+'vx.h5', fpath+'vy.h5'])
+data_key = 'igram' # B. Riel convention for access to datasets in hdf5 stack
+
+## Extract time series at selected points
+upstream_max = 500 # index of last xh,yh within given distance of terminus--pts roughly 50m apart
+xys = [(xh[i], yh[i]) for i in range(0, upstream_max, 100)]
+labels=[str(i) for i in range(len(xys))]
+series = [hel_stack.timeseries(xy=xyi, key=data_key) for xyi in xys]
