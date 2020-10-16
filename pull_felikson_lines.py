b055730b5862940715f85d68550660fd29a9ea6f
Lizz Ultee [air] <ehultee@umich.edu>
2020-10-16 16:47:15 -0400

    Add scatter to pull_felikson_lines
    Collect all necessary inversion tools in same script, pull coincident series along flowline, compute max cross-correlation and temporal lag at which this cross-corr occurs, plot along flowline.

--- pull_felikson_lines.py
+++ pull_felikson_lines.py
@@ -38,6 +38,52 @@ data_key = 'igram' # B. Riel convention for access to datasets in hdf5 stack
 
 ## Extract time series at selected points
 upstream_max = 500 # index of last xh,yh within given distance of terminus--pts roughly 50m apart
-xys = [(xh[i], yh[i]) for i in range(0, upstream_max, 100)]
-labels=[str(i) for i in range(len(xys))]
-series = [hel_stack.timeseries(xy=xyi, key=data_key) for xyi in xys]
+xys = [(xh[i], yh[i]) for i in range(0, upstream_max, 25)]
+
+# Create an evenly spaced time array for time series predictions
+t_grid = np.linspace(hel_stack.tdec[0], hel_stack.tdec[-1], 1000)
+
+# First convert the time vectors to a list of datetime
+dates = ice.tdec2datestr(hel_stack.tdec, returndate=True)
+dates_grid = ice.tdec2datestr(t_grid, returndate=True)
+
+# Build the collection
+collection = build_collection(dates)
+
+# Construct a priori covariance
+Cm = computeCm(collection)
+iCm = np.linalg.inv(Cm)
+
+# Instantiate a model for inversion
+model = ice.tseries.Model(dates, collection=collection)
+
+# Instantiate a model for prediction
+model_pred = ice.tseries.Model(dates_grid, collection=collection)
+
+## Access the design matrix for plotting
+G = model.G
+
+# Create lasso regression solver that does the following:
+# i) Uses an a priori covariance matrix for damping out the B-splines
+# ii) Uses sparsity-enforcing regularization (lasso) on the integrated B-splines
+solver = ice.tseries.select_solver('lasso', reg_indices=model.itransient, penalty=0.05,
+                                   rw_iter=1, regMat=iCm)
+
+corr_max = []
+lag_max = []
+for xy in xys:
+    pred, st, lt = VSeriesAtPoint(xy, vel_stack=hel_stack, collection=collection, 
+                                  model=model, model_pred=model_pred, solver=solver, 
+                                  t_grid=t_grid, sigma=1.5, data_key='igram')
+    corr, lags, ci = SmbXcorr(xy, smb_dictionary=SMB_dict, smb_dates=smb_dates, 
+                              velocity_pred=pred, t_grid=t_grid, diff=1)
+    corr_max.append(max(corr))
+    lag_max.append(lags[np.argmax(corr)])
+
+fig, ax = plt.subplots(1)
+ax.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=corr_max)
+plt.show()
+
+fig1, ax1 = plt.subplots(1)
+ax1.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=lag_max, cmap='plasma')
+plt.show()
