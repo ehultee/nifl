6feb2419664e26d2bf63a7bab7d7b266357b7cb7
Lizz Ultee [air] <ehultee@umich.edu>
2021-03-19 17:30:53 -0400

    Add prewhitening options
    Add functions `Arima_ResidFunc` and `XCorr1D_prewhitened` to nifl_helper.py to facilitate pre-whitening for cross-correlation analysis.  Add script `whitened_xcorr-example.py` to demonstrate.
    
    Note that for certain applications, including study of long-term trends, pre-whitening is not a good choice.

--- nifl_helper.py
+++ nifl_helper.py
@@ -8,6 +8,8 @@ from scipy import interpolate
 import collections
 import datetime
 import numpy as np
+import statsmodels.api as sm
+import pandas as pd
 import iceutils as ice
 from iceutils import tseries, timeutils
 
@@ -443,3 +445,141 @@ def Xcorr1D(pt, series_func, series_dates, velocity_pred, t_grid, t_limits, diff
     	
     return corr, lags, ci
     
+def Arima_ResidFunc(series, normalize=True, diff=1):
+    """
+    Fit an ARIMA model of appropriate order to the input series, compute the 
+    residuals we will use for the cross-correlation with velocity.
+
+    Parameters
+    ----------
+    series : pd.Series
+        The "input variable" (SMB, runoff, terminus position...) expressed as a
+        one-dimensional time series.  The timestamp of each data point 
+        should be the "index" of the pandas Series.
+    normalize : bool, optional
+        Whether to normalize series before fitting ARIMA. Default is True.
+        Normalized residuals will make the output of XCorr1D inter-comparable 
+        with normalized output for other variables.  If set to False, the signal 
+        amplitude will be larger but the correlation values may exceed 1.
+    diff : int, optional
+        Number of discrete differences to apply to data. Default is 1.
+        We do this to hone in on the effect of our variables on each other,
+        rather than a mutual response (background trend) to a shared 
+        un-modelled factor.
+
+    Returns
+    -------
+    mod_fit: statsmodels.tsa.arima.model.ARIMAResultsWrapper
+        The ARIMA model fit to the series, which we will use in filtering
+        the velocity data for cross-correlation.
+        (Ref: https://online.stat.psu.edu/stat510/lesson/9/9.1)
+    resid_func : interpolate.interp1d
+        1D-interpolated function with values of residuals (series-ARIMA) over time.
+        We will use this to sample the residuals at the same time intervals
+        as the velocity, which is necessary for the cross-correlation.
+    series_resid : pd.Series , optional
+        A pd.Series of the residuals
+
+    """
+    if normalize:
+        series_normed = (series-np.mean(series)) / (np.std(series)*len(series))
+    else:
+        series_normed = series
+    
+    ## Prewhitening data: fit ARIMA model to series data
+    d = diff # order of differencing to apply in ARIMA model 
+    ## partial autocorrelation function helps us choose the AR term (p) in ARIMA
+    lag_pacf = sm.tsa.stattools.pacf(series_normed, nlags=20, method='ols')
+    upper_confidence = 1.96/np.sqrt(len(series_normed))
+    p_candidates = np.argwhere(lag_pacf > upper_confidence).squeeze()
+    p = p_candidates[p_candidates >0][0] # choose first nonzero value that exceeds upper CI
+    ## autocorrelation function helps us choose the MA term (q) in ARIMA
+    lag_acf = sm.tsa.stattools.acf(series_normed, nlags=20) 
+    q_candidates = np.argwhere(lag_acf > upper_confidence).squeeze()
+    q = q_candidates[q_candidates >0][0] # choose first nonzero value that exceeds upper CI
+    print('Fitting ARIMA of order ({},{},{})'.format(p,d,q))
+    
+    mod = sm.tsa.arima.ARIMA(series_normed, order=(p,d,q))
+    mod_fit = mod.fit()
+
+    series_resid = pd.DataFrame(mod_fit.resid).squeeze()
+    resid_d = [d.utctimetuple() for d in series_resid.index]
+    resid_d_interp = [ice.timeutils.datestr2tdec(d[0], d[1], d[2]) for d in resid_d]
+    resid_func = interpolate.interp1d(resid_d_interp, series_resid)
+    
+    return mod_fit, resid_func, series_resid
+
+def Xcorr1D_prewhitened(resid_func, series_dates, velocity_pred, t_grid, t_limits, 
+            mod_fit, normalize=True, pos_only=False):
+    """
+    Compute cross-correlation on coincident series of a 1D time series
+    (e.g. catchment-integrated runoff or SMB) versus velocity at a point.
+    
+    Note that unlike XCorr1D, we do not include the option to difference the 
+    velocity time series here.  Rather, we apply a convolution filter defined
+    by the parameters of the ARIMA model mod_fit.
+
+    Parameters
+    ----------
+    resid_func : interpolate.interp1d
+        1D-interpolated function with values of data series over time.
+        We assume that this is already normalized.
+    series_dates : list
+        Decimal dates of data points
+    velocity_series : dict
+        Output of iceutils prediction.
+    t_grid : ndarray
+        Evenly spaced decimal times at which spline-fit velocity is sampled
+    t_limits : tuple
+        Start and end dates (decimal) of the time period to study
+    mod_fit : statsmodels.tsa.arima.model.ARIMAResultsWrapper
+        An ARIMA model fit to the input variable, which we will use in filtering
+        the velocity data for cross-correlation.
+        (Ref: https://online.stat.psu.edu/stat510/lesson/9/9.1)
+    normalize : bool, optional
+        Whether to normalize for a cross-correlation in [-1,1]. Default is True.
+        This makes the output inter-comparable with normalized output for other
+        variables.  If set to False, the signal amplitude will be larger but
+        the correlation values may exceed 1.
+    pos_only : bool, optional
+    	Whether to analyse only xcorrs with positive lag values.  Default is False.
+    	This allows a bidirectional causal relationship.  For a causal relationship 
+    	hypothesised to be single-directional, choose True to display only positive
+    	lag values.
+
+    Returns
+    -------
+    corr : array
+        Cross-correlation coefficients between SMB, velocity
+    lags : array
+        Time lag for each correlation value
+    ci : array
+        Confidence intervals for evaluation
+
+    """
+    t_min = max(min(series_dates), t_limits[0])
+    t_max = min(max(series_dates), t_limits[1])
+    coincident_dates = np.asarray([t for t in t_grid if (t>=t_min and t<t_max)])
+    coincident_resid = resid_func(coincident_dates) # sample at same dates as velocity series
+    
+    vel_series_0 = velocity_pred['full'][np.where(t_grid>=t_min)]
+    vel_series = vel_series_0[np.where(t_grid[np.where(t_grid>=t_min)]<t_max)] # trim dates to match t_limits
+    if normalize:
+        vel_series = (vel_series-np.mean(vel_series)) / (np.std(vel_series))
+    v_filt = sm.tsa.filters.convolution_filter(vel_series, filt=mod_fit.params)[1:-1]  #trim nans from ends
+    # TODO: confirm whether this whitens correctly
+
+        
+    corr = np.correlate(coincident_resid[1:-1], v_filt, mode='full')
+    lags = range(int(-0.5*len(corr)), int(0.5*len(corr)+1))
+    ci = [2/np.sqrt(len(coincident_resid)-abs(k)) for k in lags]
+
+    ## convert lags to physical units
+    lags = np.mean(np.diff(t_grid))*365.26*np.asarray(lags)
+    
+    if pos_only:
+    	corr = corr[np.argwhere(lags>=0)].squeeze()
+    	ci = np.asarray(ci)[np.argwhere(lags>=0)].squeeze()
+    	lags = lags[np.argwhere(lags>=0)].squeeze()
+    
+    return corr, lags, ci
---
+++ whitened_xcorr-example.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Use prewhitening functions in nifl_helper to recompute all xcorrs
+
+Created on Thu Mar 18 17:29:30 2021
+
+@author: lizz
+"""
+from netCDF4 import Dataset
+from scipy import interpolate
+import pyproj as pyproj
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import LightSource
+from mpl_toolkits.axes_grid1 import make_axes_locatable 
+import iceutils as ice
+import nifl_helper as nifl
+
+
+# ### Define where the necessary data lives
+
+# In[ ]:
+
+
+flowline_fpath = '/Users/lizz/Documents/GitHub/Data_unsynced/Felikson-flowlines/netcdfs/glaciera199.nc'
+velocity_fpath='/Users/lizz/Documents/GitHub/Data_unsynced/Gld-Stack/'
+gl_bed_fpath ='/Users/lizz/Documents/GitHub/Data_unsynced/BedMachine-Greenland/BedMachineGreenland-2017-09-20.nc'
+catchment_smb_fpath = '/Users/lizz/Documents/GitHub/Data_unsynced/Helheim-processed/smb_rec._.BN_RACMO2.3p2_ERA5_3h_FGRN055.1km.MM.csv'
+runoff_fpath = '/Users/lizz/Documents/GitHub/Data_unsynced/Helheim-processed/runoff._.BN_RACMO2.3p2_ERA5_3h_FGRN055.1km.MM.csv'
+termini_fpath = '/Users/lizz/Documents/GitHub/Data_unsynced/Helheim-processed/HLM_terminus_widthAVE.csv'
+
+# ### Define the domain of analysis
+
+# We will analyse along flowlines defined by Denis Felikson in his previous work, saved and shared as NetCDF files.  The flowlines are numbered 01-10 across the terminus; flowline 05 is close to the middle.  Note that Helheim Glacier has two large branches.  For now we'll study the main trunk, `glaciera199.nc`.  The more southerly trunk is `glacierb199.nc`.
+
+# In[ ]:
+
+
+ncfile = Dataset(flowline_fpath, 'r')
+xh = ncfile['flowline05'].variables['x'][:]
+yh = ncfile['flowline05'].variables['y'][:]
+ncfile.close()
+
+
+# In[ ]:
+
+
+## Define points at which to extract
+upstream_max = 500 # index of last xh,yh within given distance of terminus--pts roughly 50m apart
+xys = [(xh[i], yh[i]) for i in range(0, upstream_max, 20)][2::]
+
+# ## Import and invert velocity observations
+
+# In[ ]:
+
+## Set up combined hdf5 stack
+hel_stack = ice.MagStack(files=[velocity_fpath+'vx.h5', velocity_fpath+'vy.h5'])
+data_key = 'igram' # B. Riel convention for access to datasets in hdf5 stack
+
+
+# In[ ]:
+
+
+# Create an evenly spaced time array for time series predictions
+t_grid = np.linspace(hel_stack.tdec[0], hel_stack.tdec[-1], 1000)
+
+# First convert the time vectors to a list of datetime
+dates = ice.tdec2datestr(hel_stack.tdec, returndate=True)
+dates_grid = ice.tdec2datestr(t_grid, returndate=True)
+
+# Build the collection
+collection = nifl.build_collection(dates)
+
+# Construct a priori covariance
+Cm = nifl.computeCm(collection)
+iCm = np.linalg.inv(Cm)
+
+# Instantiate a model for inversion
+model = ice.tseries.Model(dates, collection=collection)
+
+# Instantiate a model for prediction
+model_pred = ice.tseries.Model(dates_grid, collection=collection)
+
+## Access the design matrix for plotting
+G = model.G
+
+# Create lasso regression solver that does the following:
+# i) Uses an a priori covariance matrix for damping out the B-splines
+# ii) Uses sparsity-enforcing regularization (lasso) on the integrated B-splines
+solver = ice.tseries.select_solver('lasso', reg_indices=model.itransient, penalty=0.05,
+                                   rw_iter=1, regMat=iCm)
+
+
+# Now that we are set up with our data and machinery, we'll ask the inversion to make us a continuous time series of velocity at each point we wish to study.
+
+
+
+# In[ ]:
+
+
+preds = []
+for j, xy in enumerate(xys):
+    try:
+        pred, st, lt = nifl.VSeriesAtPoint(xy, vel_stack=hel_stack, collection=collection, 
+                                  model=model, model_pred=model_pred, solver=solver, 
+                                  t_grid=t_grid, sigma=2.5, data_key='igram')
+        preds.append(pred)
+    except AssertionError: # catches failed inversion
+        print('Insufficient data for point {}. Removing'.format(j))
+        xys.remove(xy)
+        continue
+
+
+# ## Comparison data sets
+
+# In[ ]:
+# ### Catchment-integrated runoff
+
+## Read in RACMO monthly int from Denis
+runoff_racmo = pd.read_csv(runoff_fpath, index_col=0, parse_dates=True)
+runoff_tr = runoff_racmo.loc[runoff_racmo.index.year >= 2006]
+runoff = runoff_tr.loc[runoff_tr.index.year <2018]   
+
+runoff_mf, runoff_rf, runoff_resid = nifl.Arima_ResidFunc(runoff)
+resid_d = [d.utctimetuple() for d in runoff_resid.index]
+resid_d_interp = [ice.timeutils.datestr2tdec(d[0], d[1], d[2]) for d in resid_d]
+
+whitened_runoff_corr_amax = []
+whitened_runoff_lag_amax = []
+for xy, pred in zip(xys, preds):
+    corr, lags, ci = nifl.Xcorr1D_prewhitened(resid_func=runoff_rf, series_dates=resid_d_interp, 
+                              velocity_pred=pred, t_grid=t_grid, t_limits=(2009,2017), 
+                              mod_fit=runoff_mf, normalize=True, pos_only=True)
+    whitened_runoff_corr_amax.append(corr[abs(corr).argmax()])
+    whitened_runoff_lag_amax.append(lags[abs(corr).argmax()])
+
+
+# In[ ]:
+# ### Catchment-integrated SMB
+# We load in a 1D timeseries of surface mass balance integrated over the whole Helheim catchment.  This data is monthly surface mass balance from the HIRHAM5 model, integrated over the Helheim catchment defined by K. Mankoff, with processing steps (coordinate reprojection, Delaunay triangulation, nearest-neighbor search and area summing) in `catchment-integrate-smb.py`.
+
+## Read in RACMO monthly int from Denis
+smb_racmo = pd.read_csv(catchment_smb_fpath, index_col=0, parse_dates=True)
+smb_tr = smb_racmo.loc[smb_racmo.index.year >= 2006]
+smb = smb_tr.loc[smb_tr.index.year <2018] 
+
+smb_mf, smb_rf, smb_resid = nifl.Arima_ResidFunc(smb)
+resid_d = [d.utctimetuple() for d in smb_resid.index]
+resid_d_interp = [ice.timeutils.datestr2tdec(d[0], d[1], d[2]) for d in resid_d]
+
+whitened_smb_corr_amax = []
+whitened_smb_lag_amax = []
+for xy, pred in zip(xys, preds):
+    corr, lags, ci = nifl.Xcorr1D_prewhitened(resid_func=smb_rf, series_dates=resid_d_interp, 
+                              velocity_pred=pred, t_grid=t_grid, t_limits=(2009,2017), 
+                              mod_fit=smb_mf, normalize=True, pos_only=True)
+    whitened_smb_corr_amax.append(corr[abs(corr).argmax()])
+    whitened_smb_lag_amax.append(lags[abs(corr).argmax()])
+    
+
+# In[ ]:
+# ### Terminus position change
+# We import width-averaged terminus position change processed by Leigh Stearns.  These data give terminus position in km from a baseline, so they do not need to be processed into a coordinate system.
+
+
+termini = pd.read_csv(termini_fpath, index_col=0, parse_dates=True, usecols=[0,1])
+trmn = termini.loc[termini.index.year >= 2006]
+tm = trmn.loc[trmn.index.year <2017]
+
+## smooth to make more comparable with SMB and runoff--but not too much for ARIMA
+td = tm.rolling('30D').mean()
+# td1 = td.resample('M').mean()
+
+##
+termini_mf, termini_rf, termini_resid = nifl.Arima_ResidFunc(td)
+# termini_resid.plot()
+
+termini_d = [d.utctimetuple() for d in tm.index]
+tm_d_interp = [ice.timeutils.datestr2tdec(d[0], d[1], d[2]) for d in termini_d]
+
+whitened_term_corr_amax = []
+whitened_term_lag_amax = []
+for xy, pred in zip(xys, preds):
+    corr, lags, ci = nifl.Xcorr1D_prewhitened(resid_func=termini_rf, series_dates=tm_d_interp, 
+                              velocity_pred=pred, t_grid=t_grid, t_limits=(2009,2017), 
+                              mod_fit=termini_mf, normalize=True, pos_only=True)
+    whitened_term_corr_amax.append(corr[abs(corr).argmax()])
+    whitened_term_lag_amax.append(lags[abs(corr).argmax()])
+
+
+# In[ ]:
+    ## plotting
+    
+    
+div_colors = 'RdBu' # choose divergent colormap for xcorr
+lag_colors = 'PiYG' # choose divergent colormap for lag
+corrnorm_min, corrnorm_max = -0.3, 0.3
+lagnorm_min, lagnorm_max = -365, 365
+
+## set matplotlib font size defaults
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+## black-white hillshade topo underneath
+rgb2 = ls.shade(np.asarray(b_hel), cmap=plt.get_cmap('gray'), blend_mode='overlay',
+               dx=np.mean(np.diff(x_hel)), dy=np.mean(np.diff(y_hel)), vert_exag=5.)
+
+fig, ((ax1, ax2, ax3), (ax4,ax5,ax6)) = plt.subplots(nrows=2,ncols=3, figsize=(12, 8), 
+                                                     # constrained_layout=True, 
+                                                     sharex=True, sharey=True,
+                                                     gridspec_kw={'wspace':0.01})
+    
+ax1.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc1 = ax1.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_smb_corr_amax, cmap=div_colors,
+                 vmin=corrnorm_min, vmax=corrnorm_max)
+# ## set up correctly scaled colorbar
+# div1 = make_axes_locatable(ax1)
+# cax1 = div1.append_axes("right", size="5%", pad=0.1)
+# plt.colorbar(sc1, cax=cax1)
+# cb1.ax.set_title('AMax. xcorr')
+ax1.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+        ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+       ylabel='Northing [km]', title='Catchment SMB')
+
+ax2.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc2 = ax2.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_runoff_corr_amax, cmap=div_colors,
+                 vmin=corrnorm_min, vmax=corrnorm_max)
+# ## set up correctly scaled colorbar
+# div2 = make_axes_locatable(ax2)
+# cax2 = div2.append_axes("right", size="5%", pad=0.1)
+# fig.colorbar(sc2, cax=cax2)
+# cb2.ax.set_title('AMax. xcorr')
+ax2.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+      ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+       title='Catchment runoff')
+
+ax3.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc3 = ax3.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_term_corr_amax, cmap=div_colors,
+                 vmin=corrnorm_min, vmax=corrnorm_max)
+## set up correctly scaled colorbar - one for all xcorr plots
+div3 = make_axes_locatable(ax3)
+cax3 = div3.append_axes("right", size="5%", pad=0.1)
+cb3 = fig.colorbar(sc3, cax=cax3)
+cb3.ax.set_ylabel('AMax. xcorr')
+ax3.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+      ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+       title='Terminus position', aspect=1.)
+
+## SECOND ROW
+ax4.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc4 = ax4.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_smb_lag_amax, cmap=lag_colors,
+                  vmin=lagnorm_min, vmax=lagnorm_max)
+# ## set up correctly scaled colorbar
+# div4 = make_axes_locatable(ax4)
+# cax4 = div4.append_axes("right", size="5%", pad=0.1)
+# plt.colorbar(sc4, cax=cax4)
+# cb1.ax.set_title('Lag [d] at peak xcorr')
+ax4.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+      ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+      xlabel='Easting [km]', ylabel='Northing [km]')
+
+ax5.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc5 = ax5.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_runoff_lag_amax, cmap=lag_colors,
+                  vmin=lagnorm_min, vmax=lagnorm_max)
+# ## set up correctly scaled colorbar
+# div5 = make_axes_locatable(ax5)
+# cax5 = div5.append_axes("right", size="5%", pad=0.1)
+# fig.colorbar(sc5, cax=cax5)
+# cb2.ax.set_title('Lag [d] at peak xcorr')
+ax5.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+      ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+      xlabel='Easting [km]')
+
+ax6.imshow(rgb2, origin='lower', extent=(x_hel[0], x_hel[-1], y_hel[0], y_hel[-1]))
+sc6 = ax6.scatter(np.asarray(xys)[:,0], np.asarray(xys)[:,1], c=whitened_term_lag_amax, cmap=lag_colors,
+                  vmin=lagnorm_min, vmax=lagnorm_max)
+## set up correctly scaled colorbar
+div6 = make_axes_locatable(ax6)
+cax6 = div6.append_axes("right", size="5%", pad=0.1)
+cb6 = fig.colorbar(sc6, cax=cax6)
+cb6.ax.set_ylabel('Lag [d] at peak xcorr')
+ax6.set(xlim=(278000, 320000), xticks=(280000, 300000, 320000), 
+      ylim=(-2590000, -2550000), yticks=(-2590000, -2570000, -2550000), 
+       xticklabels=('280', '300', '320'), yticklabels=('-2590', '-2570', '-2550'),
+      xlabel='Easting [km]', aspect=1.)
+# plt.tight_layout()
+# plt.show()
+plt.savefig('/Users/lizz/Desktop/20210204-helheim-xcorr_lag_composite')
